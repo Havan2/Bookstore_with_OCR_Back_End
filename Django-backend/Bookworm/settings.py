d788fe6727f1c2a599d106e744c58674a31cced7
Kshitij Bhatt <kshitijbhatt1998@gmail.com>
2024-04-10 14:45:00 -0400

    database

--- Django-backend/Bookworm/settings.py
+++ Django-backend/Bookworm/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'Bookworm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tomeswap',
+        'NAME': 'bookworm',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
